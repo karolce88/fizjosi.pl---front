4c6caed743d164fd1309d0e865c0c4e2279c7830
Karol <karolcwiklik@o2.pl>
2025-11-14 20:04:10 +0100

    fizjosi

---
+++ src/js/main.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+// document.querySelectorAll('.menu-item-has-children').forEach(item => {
+//   item.addEventListener('click', e => {
+//     item.classList.toggle('open');
+//   });
+
+//   // hover
+//   item.addEventListener('mouseenter', () => {
+//     item.classList.add('open');
+//   });
+
+//   item.addEventListener('mouseleave', () => {
+//     item.classList.remove('open');
+//   });
+// });
+
+
+
+// ---------- Obsługa roku w footerze -----------
+const footerYear = document.querySelector('.JSyear')
+const handleCurrentYear = () => {
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year;
+}
+handleCurrentYear();
+
+});
